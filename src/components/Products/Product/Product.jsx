081f2f719fea8b264b15b4f09869751bbe3e3465
Lakshya <labhansh40527.com>
2023-04-07 11:48:37 +0530

    Product section modified

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -1,6 +1,22 @@
 import "./Product.scss";
+
+import prod from "../../../assets/products/earbuds-prod-1.webp";
+
+
+
 const Product = () => {
-    return <div>Product</div>;
+    return (
+        <div className="product-card">
+            <div className="thumbnail">
+                <img src={prod}  alt="" />
+            </div>
+            <div className="prod-details">
+                <div className="name">ProductName</div>
+                <div className="price">&#8377;4999</div>
+            </div>
+        </div>
+    );
 };
 
 export default Product;
+
