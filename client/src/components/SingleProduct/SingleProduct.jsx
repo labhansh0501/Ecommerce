9fd2d88a7a268c1c757753133a6c3b695a443904
Lakshya <labhansh40527.com>
2023-04-11 12:52:16 +0530

    single product page modified

--- client/src/components/SingleProduct/SingleProduct.jsx
+++ client/src/components/SingleProduct/SingleProduct.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import RelatedProducts from "./RelatedProducts/RelatedProducts";
 import {
   FaFacebookF,
@@ -15,8 +16,21 @@ import useFetch from "../../Hooks/useFetch";
 import { useParams } from "react-router-dom";
 
 const SingleProduct = () => {
+  const [quantity,  setQuantity] = useState(1);
   const { id } = useParams();
   const { data } = useFetch(`/api/products?populate=*&[filters] [id]=${id}`);
+
+  const increment = () => {
+    setQuantity((prevState) => prevState + 1);
+  };
+
+  const decrement = () => {
+    setQuantity((prevState) => {
+        if(prevState === 1) return 1;
+        return prevState - 1;
+    });
+  };
+
 
   if (!data) return;
   const product = data.data[0].attributes;
@@ -27,7 +41,7 @@ const SingleProduct = () => {
           <div className="left">
             <img
               src={
-                process.env.REACT_APP_DEV_URl +
+                process.env.REACT_APP_DEV_URL +
                 product.img.data[0].attributes.url
               }
               alt=""
@@ -40,9 +54,9 @@ const SingleProduct = () => {
 
             <div className="cart-buttons">
               <div className="quantity-buttons">
-                <span>-</span>
-                <span>5</span>
-                <span>+</span>
+                <span onClick={decrement}>-</span>
+                <span>{quantity}</span>
+                <span onClick={increment}>+</span>
               </div>
               <button className="add-to-cart-button">
                 <FaCartPlus size={20} />
@@ -54,13 +68,13 @@ const SingleProduct = () => {
 
             <div className="info-item">
               <span className="text-bold">
-                Category:
-                <span>HeadPhones</span>
+                Category:{" "}
+                <span>{product.categories.data[0].attributes.title}</span>
               </span>
               <span className="text-bold">
                 Share:
                 <span className="social-icons">
-                  <FaFacebookF size={16} />
+                  <FaFacebookF size={16} /> 
                   <FaTwitter size={16} />
                   <FaInstagram size={16} />
                   <FaLinkedin size={16} />
