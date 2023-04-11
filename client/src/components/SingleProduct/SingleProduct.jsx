edf758ceb78e182775e03dff8b04a2120ff33e07
Lakshya <labhansh40527.com>
2023-04-11 09:51:27 +0530

    Single Producst page modified

--- client/src/components/SingleProduct/SingleProduct.jsx
+++ client/src/components/SingleProduct/SingleProduct.jsx
@@ -1,65 +1,79 @@
 import RelatedProducts from "./RelatedProducts/RelatedProducts";
 import {
-    FaFacebookF,
-    FaTwitter,
-    FaInstagram,
-    FaLinkedin,
-    FaPinterest,
-    FaCartPlus
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedin,
+  FaPinterest,
+  FaCartPlus,
 } from "react-icons/fa";
 
 import prod from "../../assets/products/earbuds-prod-1.webp";
 import "./SingleProduct.scss";
 
+import useFetch from "../../Hooks/useFetch";
+import { useParams } from "react-router-dom";
+
 const SingleProduct = () => {
-    return (
-        <div className="single-product-main-content">
-            <div className="layout">
-                <div className="single-product-page">
-                    <div className="left">
-                        <img src={prod} alt="" />
-                    </div>
-                    <div className="right">
-                        <span className="name">Product name</span>
-                        <span className="price">Price</span>
-                        <span className="desc">Product description</span>
+  const { id } = useParams();
+  const { data } = useFetch(`/api/products?populate=*&[filters] [id]=${id}`);
 
-                        <div className="cart-buttons">
-                            <div className="quantity-buttons">
-                                <span>-</span>
-                                <span>5</span>
-                                <span>+</span>
-                            </div>
-                            <button className="add-to-cart-button">
-                                <FaCartPlus size={20} />
-                                ADD TO CART
-                            </button>
-                        </div>
+  if (!data) return;
+  const product = data.data[0].attributes;
+  return (
+    <div className="single-product-main-content">
+      <div className="layout">
+        <div className="single-product-page">
+          <div className="left">
+            <img
+              src={
+                process.env.REACT_APP_DEV_URl +
+                product.img.data[0].attributes.url
+              }
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <span className="name">{product.title}</span>
+            <span className="price"> &#8377;{product.price}</span>
+            <span className="desc">{product.desc}</span>
 
-                        <span className="divider" />
-
-                        <div className="info-item">
-                            <span className="text-bold">
-                               Category:
-                               <span>HeadPhones</span>
-                            </span>
-                            <span className="text-bold">
-                               Share:
-                               <span className="social-icons">
-                                   <FaFacebookF size={16} />
-                                   <FaTwitter size={16} />
-                                   <FaInstagram size={16} />
-                                   <FaLinkedin size={16}  />
-                                   <FaPinterest size={16} />
-                               </span>
-                            </span>
-                        </div>
-                    </div>
-                </div>
-                <RelatedProducts />
+            <div className="cart-buttons">
+              <div className="quantity-buttons">
+                <span>-</span>
+                <span>5</span>
+                <span>+</span>
+              </div>
+              <button className="add-to-cart-button">
+                <FaCartPlus size={20} />
+                ADD TO CART
+              </button>
             </div>
+
+            <span className="divider" />
+
+            <div className="info-item">
+              <span className="text-bold">
+                Category:
+                <span>HeadPhones</span>
+              </span>
+              <span className="text-bold">
+                Share:
+                <span className="social-icons">
+                  <FaFacebookF size={16} />
+                  <FaTwitter size={16} />
+                  <FaInstagram size={16} />
+                  <FaLinkedin size={16} />
+                  <FaPinterest size={16} />
+                </span>
+              </span>
+            </div>
+          </div>
         </div>
-    );
+        <RelatedProducts />
+      </div>
+    </div>
+  );
 };
 
 export default SingleProduct;
