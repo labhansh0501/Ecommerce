6b77955d5d6543d4e55a634a55b393349eb3f52f
Lakshya <labhansh40527.com>
2023-04-12 13:53:55 +0530

    payment gatreway

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -17,4 +17,11 @@ export const fetchDataFromApi = async (url) => {
         console.log(error)
         return error;
     }
-}
+};
+
+export const makePaymentRequest = axios.create({
+    baseURL : process.env.REACT_APP_DEV_URL,
+    headers: {
+        Authorization : "bearer" + process.env.REACT_APP_STRIPE_APP_KEY,
+    },
+});
